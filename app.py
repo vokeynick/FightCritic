0c0e82f4ef08856a73e7510d86b90beecc7dff60
Nick Vokey <vokeynick@gmail.com>
2020-12-03 14:38:07 -0500

    added app.py

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-from myproject import app,db
-from flask import render_template, redirect, request, jsonify, url_for, flash, abort, request
+from myproject import app, db, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_DISCOVERY_URL, client, facebook, OAuthException
+from flask import render_template, redirect, request, jsonify, url_for, flash, abort, request, session
 from flask_login import login_user, login_required, logout_user, current_user
 from datetime import datetime
 import uuid
+import requests
+import json
 from sqlalchemy.sql import func
 from myproject.models import User, Comment, Review, Fight, FightCard
 from myproject.forms import LoginForm, RegistrationForm, CommentForm, ReviewForm, FightForm
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask_dance.consumer import oauth_authorized, oauth_error
 
 @app.route('/')
 def home():
     return render_template('home.html', cards=FightCard.query.order_by(FightCard.id.desc()).all(),      #list of cards for main page
                                         maincard=FightCard.query.order_by(FightCard.id.desc()).first())     #grab most recent card for main image
-
+@app.route('/fuckoff')
+def fuckoff():
+    pass
 
 @app.route('/fight_card/<int:card_id>')
 def fight_card(card_id):
@@ -31,6 +35,10 @@ def fight(fight_id):
     fight_url = request.url
     rating = Review.query.with_entities(func.avg(Review.rating)).filter(Review.fight_id == fight.id).scalar()
     if reviewform.submitreview.data and reviewform.is_submitted():
+        review_exists = Review.query.filter_by(user_id=current_user.username, fight_id=fight.id).first()
+        if review_exists:
+            flash("You've already left a review for this fight!")
+            return redirect(url_for('fight', fight_id=fight_id))
         review = Review(rating=reviewform.rating.data,
                         title=reviewform.title.data,
                         content=reviewform.content.data,
@@ -71,7 +79,7 @@ def login():
             return redirect(url_for('login'))
         if user.check_password(form.password.data) and user is not None:
 
-            login_user(user)
+            login_user(user, remember=True)
             flash('Logged in successfully.')
 
             next = request.args.get('next')
@@ -102,23 +110,30 @@ def register():
                     password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Thanks for registering! Now you can login!')
-        return redirect(url_for('login'))
+        login_user(user, remember=True)
+        flash('Thanks for registering!')
+        return redirect(url_for('home'))
     return render_template('register.html', form=form)
+
 def get_google_provider_cfg():
     return requests.get(GOOGLE_DISCOVERY_URL).json()
 
 @app.route("/google_login")
 def google_login():
+    # Find out what URL to hit for Google login
     google_provider_cfg = get_google_provider_cfg()
     authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
     request_uri = client.prepare_request_uri(authorization_endpoint,
                                              redirect_uri=request.base_url + "/callback",
                                              scope=["openid", "email", "profile"])
     return redirect(request_uri)
-
+# create/login local user on successful OAuth login
 @app.route("/google_login/callback")
 def callback():
+    # Get authorization code Google sent back to you
     code = request.args.get("code")
     google_provider_cfg = get_google_provider_cfg()
     token_endpoint = google_provider_cfg["token_endpoint"]
@@ -187,7 +202,10 @@ def facebook_authorized():
         db.session.commit()
         login_user(user, remember=True)
     flash('You are logged in with Facebook!')
-    return redirect(url_for('home'))
+    next = request.args.get('next')
+    if next == None:
+        next = url_for('home')
+    return redirect(next)
 
 @facebook.tokengetter
 def get_facebook_oauth_token():
